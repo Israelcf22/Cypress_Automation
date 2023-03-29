cb0897cd31a61115b48a0d9336b9679b4a527cb8
Israelcf22 <israelsfw22@gmail.com>
2023-03-28 21:35:34 -0700

    modificando clase principal

--- cypress/e2e/3-FreeRangeTesters/HomePage.cy.js
+++ cypress/e2e/3-FreeRangeTesters/HomePage.cy.js
@@ -9,10 +9,8 @@ describe('Home de www.frerangetesters.com', () => {
    it('should have a tittle', () => {
     //cy.wait(6000)
     cy.title().should('include','Free Range Testers')
-    cy.xpath('//*[@id="comp-l02x1m8d2label"]').click() 
+    cy.xpath('//*[@id="comp-l02x1m8d2label"]').click() //se agrega programa para poder buscar por XPATH
     cy.contains('Iniciar Sesión').click()
-    cy.contains
-    
    });
 
 
